46b79de95ca0aefa5f7577ad4a20acd8a96b28e5
Matteo <matteomerv@gmail.com>
2018-10-22 18:38:23 +0200

    try change

--- libft/incs/get_next_line.h
+++ libft/incs/get_next_line.h
@@ -6,13 +6,16 @@
 /*   By: matteo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/12 15:25:06 by matteo            #+#    #+#             */
-/*   Updated: 2018/05/30 13:14:46 by mmervoye         ###   ########.fr       */
+/*   Updated: 2018/10/22 18:38:09 by mmervoye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # define BUFF_SIZE 4096
+#ifndef OPEN_MAX
+# define OPEN_MAX 256
+#endif
 # include <limits.h>
 # include <stdlib.h>
 # include "libft.h"
